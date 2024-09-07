26b21481e4942619bb972dbcfd9c467a9b9c715b
HardikAcumen <hprajapati@chuckads.com>
2024-09-07 17:48:08 +0530

    first

---
+++ src/components/header.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+export const Header = (props) => {
+  return (
+    <header id="header">
+      <div className="intro">
+        <div className="col-md-8 col-md-offset-2 intro-text">
+          <h1 style={{color : "orange"}}>
+            {props.data ? props.data.title : "Loading"}
+            <span></span>
+          </h1>
+          <p style={{color : "orange"}}>{props.data ? props.data.paragraph : "Loading"}</p>
+          <a
+            href="#features"
+            className="btn btn-custom btn-lg page-scroll"
+          >
+            Learn More
+          </a>{" "}
+        </div>
+      </div>
+    </header>
+  );
+};
